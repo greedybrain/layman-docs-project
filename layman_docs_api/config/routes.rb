9954f4d887a948757e231f6b8998a6b96fa74342
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-05-07 09:09:22 -0400

    config routes to work with devise sessions and reg

--- layman_docs_api/config/routes.rb
+++ layman_docs_api/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :laymen
-
+  devise_for :laymen,
+    path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    controllers: {
+      sessions: 'sessions',
+      registrations: 'registrations'
+    }
 
   root to: "home#index"
 end
