c9dec5e9936323f905aeeeac9b4c0698c86eaccf
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-05-20 11:26:23 -0400

    fix fetch

--- frontend_client/src/Events/app_event_post.js
+++ frontend_client/src/Events/app_event_post.js
@@ -30,8 +30,8 @@ class AppEventPost {
                     "animate__slideOutLeft",
                     "animate__fast"
                )
+               App.refresh(1000)
           })
-          App.refresh(500)
      }
 
      static openCloseLaymensPosts() {
